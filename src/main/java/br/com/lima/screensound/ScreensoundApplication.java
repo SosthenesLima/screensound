622609c2b6150b3708c1005f54723f4a7e125912
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-01-06 10:35:15 -0300

    Commit Configurando as bases do projeto01

--- src/main/java/br/com/lima/screensound/ScreensoundApplication.java
+++ src/main/java/br/com/lima/screensound/ScreensoundApplication.java
@@ -1,5 +1,6 @@
 package br.com.lima.screensound;
 
+import br.com.lima.screensound.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ public class ScreensoundApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
