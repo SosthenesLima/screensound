017d55c1d501baf1d0eaca6415a299313a640c17
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2025-01-08 13:18:07 -0300

    Commit Salvando músicas e artistas no banco de dados03

--- src/main/java/br/com/lima/screensound/principal/Principal.java
+++ src/main/java/br/com/lima/screensound/principal/Principal.java
@@ -2,26 +2,32 @@ package br.com.lima.screensound.principal;
 
 import br.com.lima.screensound.model.Artista;
 import br.com.lima.screensound.model.TipoArtista;
+import br.com.lima.screensound.repository.ArtistaRepository;
 
 import java.util.Scanner;
 
 public class Principal {
 
+    private final ArtistaRepository repositorio;
     private Scanner leitura = new Scanner(System.in);
+
+    public Principal(ArtistaRepository repositorio) {
+        this.repositorio = repositorio;
+    }
 
     public void exibeMenu() {
         var opcao = -1;
 
-        while (opcao!= 9) {
+        while (opcao != 9) {
             var menu = """
                     *** Screen Sound Músicas ***                    
-                                        
+                    
                     1- Cadastrar artistas
                     2- Cadastrar músicas
                     3- Listar músicas
                     4- Buscar músicas por artistas
                     5- Pesquisar dados sobre um artista
-                                    
+                    
                     9 - Sair
                     """;
 
@@ -67,13 +73,20 @@ public class Principal {
     }
 
     private void cadastrarArtistas() {
-        System.out.println("Informe o nome desse Artista: ");
-        var nome = leitura.nextLine();
-        System.out.println("Informe o tipo desse artista: (solo, dupla ou banda)");
-        var tipo = leitura.nextLine();
-        TipoArtista tipoArtista = TipoArtista.valueOf(tipo.toUpperCase());
-        Artista artista = new Artista(nome, tipoArtista);
-        repositorio.save(artista);
+        var cadastrarNovo = "S";
+
+        while (cadastrarNovo.equalsIgnoreCase("s")) {
+            System.out.println("Informe o nome desse Artista: ");
+            var nome = leitura.nextLine();
+            System.out.println("Informe o tipo desse artista: (solo, dupla ou banda)");
+            var tipo = leitura.nextLine();
+            TipoArtista tipoArtista = TipoArtista.valueOf(tipo.toUpperCase());
+            Artista artista = new Artista(nome, tipoArtista);
+            repositorio.save(artista);
+            System.out.println("Cadastrar novo artista?  (S/N)");
+            cadastrarNovo = leitura.nextLine();
+        }
+
     }
 
 }
